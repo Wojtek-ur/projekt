873721d2aa70f297ed66d22987f7e321d88eb910
Your Name <you@example.com>
2024-06-29 16:04:39 +0200

    dziala

---
+++ src/Edge.java
@@ -0,0 +1,23 @@
+public class Edge {
+    private Node startNode;
+    private Node endNode;
+    private boolean directed; // Flag to indicate if the edge is directed
+
+    public Edge(Node startNode, Node endNode, boolean directed) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.directed = directed;
+    }
+
+    public Node getStartNode() {
+        return startNode;
+    }
+
+    public Node getEndNode() {
+        return endNode;
+    }
+
+    public boolean isDirected() {
+        return directed;
+    }
+}
